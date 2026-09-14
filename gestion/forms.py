0b7ff1cb2f5f0e96cb1552396188ebfcc6439b32
Cesar Astorga <cesar.antonio.astorga@gmail.com>
2026-09-14 13:00:50 -0300

    Feliz 18 para todos :D

--- gestion/forms.py
+++ gestion/forms.py
@@ -1,4 +1,5 @@
-from django import forms 
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Cliente
 
 class ClienteForm(forms.ModelForm):
@@ -9,3 +10,41 @@ class ClienteForm(forms.ModelForm):
             'email',
             'telefono'
         ]
+        widgets = {
+            'nombre': forms.TelInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder':'Nombre completo'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder':'correo@ejemplo.com'
+                }
+            ),
+            'telefono': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder':'+56 9 1234 5678'
+                }
+            )
+        }
+        
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Usuario'
+            }
+        )
+    )
+    password = forms.CharField(
+            widget=forms.PasswordInput(
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'Contrasena'
+                }
+            )
+        )
